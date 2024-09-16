5d715aa1780291f7468e43d2f021d6a8e3bf6e3d
LaurentArcos <laurent.arcos@gmail.com>
2024-09-16 17:42:31 +0200

    modification vite config avec ajout de la config pour mise en prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,46 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+ import { defineConfig } from 'vite';
+ import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'custom-logger',
-      configureServer(server) {
-        server.httpServer?.on('listening', () => {
-          console.log('🟢 Votre serveur Vite est démarré et écoute sur http://localhost:5173');
-        });
-      },
-    },
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-});
+ export default defineConfig({
+   plugins: [
+     react(),
+     {
+       name: 'custom-logger',
+       configureServer(server) {
+         server.httpServer?.on('listening', () => {
+           console.log('🟢 Votre serveur Vite est démarré et écoute sur http:localhost:5173');
+         });
+       },
+     },
+   ],
+   server: {
+     proxy: {
+       '/api': {
+         target: 'http:localhost:8080',
+         changeOrigin: true,
+         secure: false,
+         rewrite: (path) => path.replace(/^\/api/, '')
+       }
+     }
+   }
+ });
+
+//* INVERSER LES COMMS selon dev ou live
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   base: '/wow-helper/', // Définit le chemin de base
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://www.laurentarcos.fr', // Change cela pour correspondre à ta configuration
+//         changeOrigin: true,
+//         secure: true,
+//         rewrite: (path) => path.replace(/^\/api/, '') // Retire le préfixe '/api'
+//       }
+//     }
+//   }
+// });
